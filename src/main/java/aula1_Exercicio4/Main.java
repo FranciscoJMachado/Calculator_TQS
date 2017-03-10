57a2b40c48c854a781faa039d18bb05b87f06f86
Francisco Machado <francisco_machado@me.com>
2017-03-10 17:53:47 +0000

    Funçao da Soma adicionada pela Alice

--- src/main/java/aula1_Exercicio4/Main.java
+++ src/main/java/aula1_Exercicio4/Main.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class Main {
 
-	public static Scanner sc =new Scanner (System.in);
+	private static final Scanner sc =new Scanner (System.in);
 	
 	public static void main(String[] args) {
 		
@@ -12,9 +12,33 @@ public class Main {
 		 ReadAndStore readandstore = new ReadAndStore(Path_to_File);
 		 
 		 ConversorPalavraParaNumero CPPN = new ConversorPalavraParaNumero(readandstore.ReadToMap());
-	
-		 String paraConverter="A list of numbers: eight million two hundred thousand five hundred twenty-four";
-		 System.out.println(CPPN.converter_para_String(paraConverter)+"-> "+CPPN.converter_para_Numero(paraConverter));
+                 
+                 System.out.println("Numero Pretendido");
+                 String paraConverter= sc.nextLine();
+                   
+                 System.out.println("Pretende o resultado da Soma? (Y/N)");
+                 String tmp = sc.nextLine();
+                 
+            switch (tmp) {
+                case "N":
+                case "No":
+                    System.out.println(CPPN.converter_para_String(paraConverter)+"-> "+CPPN.converter_para_Numero(paraConverter));
+                    break;
+                case "Y":
+                case "Yes":
+                    System.out.println("Segundo Numero Pretendido");
+                    String paraConverter2 = sc.nextLine();
+                    //BUG: Troca do + pelo -
+                    int resultado = Integer.parseInt(CPPN.converter_para_Numero(paraConverter)) - Integer.parseInt(CPPN.converter_para_Numero(paraConverter2));
+                    System.out.println(CPPN.converter_para_Numero(paraConverter) +"+"+ CPPN.converter_para_Numero(paraConverter2) +"= "+resultado);
+                    break;
+                default:
+                    System.err.println("Nenhuma resposta válida");
+                    break;
+            }
+               
+    
+		
 	}
 
 }
